7501ec7a25c9a546e17bbfedb4858d7327300514
tobibanks <tobiodogwu@gmail.com>
2024-10-26 01:59:09 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import JobsPage from "./pages/JobsPage"
 import NotFoundPage from "./pages/NotFoundPage"
 import JobPage, { jobLoader } from "./pages/JobPage"
 import AddJobPage from "./pages/AddJobPage"
+import EditJobPage from "./pages/EditJobPage"
 
 const App = () => {
   // Add New Job
@@ -52,6 +53,11 @@ const App = () => {
         <Route path="/jobs" element={<JobsPage />} />
         <Route path="/add-job" element={<AddJobPage addJobSubmit={addJob} />} />
         <Route path="/jobs/:id" element={<JobPage />} loader={jobLoader} />
+        <Route
+          path="/edit-job/:id"
+          element={<EditJobPage updateJobSubmit={updateJob} />}
+          loader={jobLoader}
+        />
         <Route path="*" element={<NotFoundPage />} />
       </Route>
     )
